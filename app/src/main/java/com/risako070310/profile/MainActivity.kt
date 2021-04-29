b4d2ec1c980858b16a6fe8f8c01a7fbab17027d8
risako070310 <risako070310@gmail.com>
2021-04-29 17:58:01 +0900

    done randy's profile

--- app/src/main/java/com/risako070310/profile/MainActivity.kt
+++ app/src/main/java/com/risako070310/profile/MainActivity.kt
@@ -2,7 +2,6 @@ package com.risako070310.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,12 +9,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        profileImage.isVisible = false
-        profileCommentText.isVisible = false
+        button1.setOnClickListener {
+            profileImage.setImageResource(R.drawable.randy_image)
+            profileLabelText.text = "名前"
+            profileCommentText.text = "Androidメンターのランディだよ"
+        }
 
-        button.setOnClickListener {
-            profileImage.isVisible = true
-            profileCommentText.isVisible = true
+        button2.setOnClickListener {
+            profileImage.setImageResource(R.drawable.baseball_image)
+            profileLabelText.text = "スポーツ"
+            profileCommentText.text = "野球観戦が好きで、スタジアムに通っている"
+        }
+
+        button3.setOnClickListener {
+            profileImage.setImageResource(R.drawable.donut_image)
+            profileLabelText.text = "好きな食べ物"
+            profileCommentText.text = "キャンディやドーナツが大好き"
+        }
+
+        button4.setOnClickListener {
+            profileImage.setImageResource(R.drawable.gadget_image)
+            profileLabelText.text = "趣味"
+            profileCommentText.text = "ガジェットを集めて動かすこと"
         }
 
     }
