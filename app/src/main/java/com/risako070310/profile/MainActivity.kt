6a15e25b819bc47b43d59cc5548eddb19bbb2b9a
risako070310 <risako070310@gmail.com>
2021-04-30 10:34:55 +0900

    done randy's profile challenge mission

--- app/src/main/java/com/risako070310/profile/MainActivity.kt
+++ app/src/main/java/com/risako070310/profile/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.risako070310.profile
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,24 +15,51 @@ class MainActivity : AppCompatActivity() {
             profileImage.setImageResource(R.drawable.randy_image)
             profileLabelText.text = "名前"
             profileCommentText.text = "Androidメンターのランディだよ"
+
+            button1.backgroundTintList = ColorStateList.valueOf(Color.rgb(0, 180, 220))
+
+            button2.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150, 150))
+            button3.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150, 150))
+            button4.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150, 150))
         }
 
         button2.setOnClickListener {
             profileImage.setImageResource(R.drawable.baseball_image)
             profileLabelText.text = "スポーツ"
             profileCommentText.text = "野球観戦が好きで、スタジアムに通っている"
+
+            button2.backgroundTintList = ColorStateList.valueOf(Color.rgb(220, 0, 100))
+
+            button1.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150, 150))
+            button3.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150, 150))
+            button4.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150, 150))
+
         }
 
         button3.setOnClickListener {
             profileImage.setImageResource(R.drawable.donut_image)
             profileLabelText.text = "好きな食べ物"
             profileCommentText.text = "キャンディやドーナツが大好き"
+
+            button3.backgroundTintList = ColorStateList.valueOf(Color.rgb(0, 180, 120))
+
+            button1.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150, 150))
+            button2.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150, 150))
+            button4.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150, 150))
+
         }
 
         button4.setOnClickListener {
             profileImage.setImageResource(R.drawable.gadget_image)
             profileLabelText.text = "趣味"
             profileCommentText.text = "ガジェットを集めて動かすこと"
+
+            button4.backgroundTintList = ColorStateList.valueOf(Color.rgb(220, 100, 0))
+
+            button1.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150, 150))
+            button2.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150, 150))
+            button3.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150, 150))
+
         }
 
     }
